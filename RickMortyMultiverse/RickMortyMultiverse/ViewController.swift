f372a65ad9c8c52fa763aa3178affaece746c804
pedroboga <pedroboga@gmail.com>
2021-10-01 10:20:01 -0300

    Criado esqueleto do projeto

---
+++ RickMortyMultiverse/RickMortyMultiverse/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  RickMortyMultiverse
+//
+//  Created by Pedro Boga on 01/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBrown
+    }
+}
+
